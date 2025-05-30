324feadc0e08c23e5b8dd21ee4d8f3b78fe3f0f4
LAB27_USER ON WS39 <L27X15W39@citulab.net>
2025-05-30 17:35:39 +0800

    first

---
+++ src/patuti/Platform.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import plat from '../images/area.png'
+import './css/platform.css'
+
+function Platform() {
+  return (
+    <div>
+      <img className = "platform"src={plat} alt="Area" /> 
+    </div>
+  )
+}
+
+export default Platform
